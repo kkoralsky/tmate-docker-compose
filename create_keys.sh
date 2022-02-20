d90849cff82c0d3f09d1c491904987d25517db80
Karol Ossowski <k@koralsky.pl>
2022-02-20 12:41:37 +0100

    use env in create_keys script

--- create_keys.sh
+++ create_keys.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -eu
 
+source .env
+
 gen_key() {
-  keytype=$1
-  ks="${keytype}_"
-  key="keys/ssh_host_${ks}key"
-  if [ ! -e "${key}" ] ; then
-    ssh-keygen -t ${keytype} -f "${key}" -N ''
-    echo ""
-  fi
-  SIG=$(ssh-keygen -l -E SHA256 -f $key.pub | cut -d ' ' -f 2)
+	keytype=$1
+	ks="${keytype}_"
+	key="keys/ssh_host_${ks}key"
+	if [ ! -e "${key}" ]; then
+		ssh-keygen -t ${keytype} -f "${key}" -N ''
+		echo ""
+	fi
+	SIG=$(ssh-keygen -l -E SHA256 -f $key.pub | cut -d ' ' -f 2)
 }
 
 mkdir -p keys
@@ -18,10 +20,9 @@ RSA_SIG=$SIG
 gen_key ed25519
 ED25519_SIG=$SIG
 
-
 echo "You may use the following settings this in your .tmate.conf:"
 echo ""
-echo "set -g tmate-server-host localhost"
-echo "set -g tmate-server-port 22"
+echo "set -g tmate-server-host $EXTERNAL_FQDN"
+echo "set -g tmate-server-port $SSH_PORT"
 echo "set -g tmate-server-rsa-fingerprint \"$RSA_SIG\""
 echo "set -g tmate-server-ed25519-fingerprint \"$ED25519_SIG\""
